905ac5cf407acd2610bd3e9ba5b97772c18f6ed0
Muhamad Kharis Ihsan <89218733+icunicn@users.noreply.github.com>
2025-07-02 00:29:43 +0700

    Delete run.bat

--- run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting Spring Boot Application...
-echo.
-echo Navigate to: http://localhost:8080
-echo.
-echo Press Ctrl+C to stop the application
-echo.
-.\mvnw.cmd spring-boot:run
-pause
